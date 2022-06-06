c6e4411051164af4c3264e1e328877372ab8cd7f
邓均源 <jy20220520@gmail.com>
2022-06-06 20:23:24 +0800

    使用swagger自动生成api文档

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -8,13 +8,23 @@ import {
   Body,
   Query,
 } from '@nestjs/common';
+
+import { ApiBearerAuth, ApiTags, ApiQuery, ApiResponse } from '@nestjs/swagger';
+
 import { HelloService } from './hello.service';
 
+@ApiBearerAuth()
+@ApiTags('hello')
 @Controller('/hello')
 export class HelloController {
   constructor(private readonly helloService: HelloService) {}
 
   @Get()
+  @ApiQuery({ name: 'id', required: true })
+  @ApiResponse({
+    status: 200,
+    description: 'get...',
+  })
   fetch(@Query() { id }): string {
     return this.helloService.fetch(id);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 设置swagger文档配置
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('zqlc_nest api document')
+    .setDescription('nest project api document')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

372e5b7d2c4b872bd179c22326bdae03bf9ba8f8
邓均源 <jy20220520@gmail.com>
2022-06-07 10:06:37 +0800

    添加logger中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [HelloModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({
+        path: 'hello',
+        method: RequestMethod.POST,
+      })
+      .forRoutes('hello');
+  }
+}
